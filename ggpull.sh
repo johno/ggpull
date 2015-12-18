333e9b21095925c30f57f890ad8f9e0ee271a7a2
John Otander <johnotander@gmail.com>
2015-12-17 22:31:00 -0700

    Specify origin

--- ggpull.sh
+++ ggpull.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 currBranch=$(eval "$(node -e "var path = require('path');console.log(path.join(path.dirname(require('fs').realpathSync('$0')), 'node_modules/.bin/git-current-branch'))")")
-git pull $currBranch
+git pull origin $currBranch
